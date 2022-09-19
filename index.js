ee39807b5bd7d79ab26a9bfb3adf68e31088f7ef
retrogamesnetwork <108418893+retrogamesnetwork@users.noreply.github.com>
2022-09-19 19:01:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-  var firebaseConfig = {
-      apiKey: "AIzaSyCYgieNluU4atVEYhCcUY5GC78eqYXBYsA",
-      authDomain: "retro-games-network.firebaseapp.com",
-      databaseURL: "https://retro-games-network-default-rtdb.firebaseio.com",
-      projectId: "retro-games-network",
-      storageBucket: "retro-games-network.appspot.com",
-      messagingSenderId: "1025542945363",
-      appId: "1:1025542945363:web:c4d81a05a8a9d8809564a6"
-    };
-  
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
 
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// initialize database
 const db = firebase.database();
 
-const chat_username = prompt("Enter a Username for the chatroom");
+// get user's data
+const username = prompt("Please Tell Us Your Name");
 
+// submit form
+// listen for submit event on the form and call the postChat function
 document.getElementById("message-form").addEventListener("submit", sendMessage);
 
+// send message to db
 function sendMessage(e) {
   e.preventDefault();
 
@@ -35,9 +41,21 @@ function sendMessage(e) {
 
   // create db collection and send in the data
   db.ref("messages/" + timestamp).set({
-    chat_username,
+    username,
     message,
   });
 }
 
+// display the messages
+// reference the collection created earlier
 const fetchChat = db.ref("messages/");
+
+// check for new messages using the onChildAdded event listener
+fetchChat.on("child_added", function (snapshot) {
+  const messages = snapshot.val();
+  const message = `<li class=${
+    username === messages.username ? "sent" : "receive"
+  }><span>${messages.username}: </span>${messages.message}</li>`;
+  // append the message on the page
+  document.getElementById("messages").innerHTML += message;
+});
