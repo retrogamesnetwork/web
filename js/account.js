e6cec6179e3713bdf50ac81ff3d2851e2446f843
retrogamesnetwork <108418893+retrogamesnetwork@users.noreply.github.com>
2022-09-18 20:09:59 -0400

    Update account.js

--- js/account.js
+++ js/account.js
@@ -1,8 +1,12 @@
 var login_button = document.getElementById("login-btn");
-var username = document.getElementById("floatingInput").value;
+var username = ""
 var welcome_message_text = document.getElementById("welcome-message");
 
 function loadProfile() {
   velcome_message_text.innerHTML = "Welcome Back, " + username + "!";
   console.log("Profile Loaded");
 }
+login_button.addEventListener("click", function() {
+  window.location.href = "https://retrogamesnetwork.github.io/web/launch.html";
+  username = document.getElementById("floatingInput").value;
+});
